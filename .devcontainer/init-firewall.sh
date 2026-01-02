51ac69fb63003ed5d498a8214f94ffe5980bbc96
ryoooo <maekwnnn@gmail.com>
2026-01-02 15:56:53 +0900

    Allow PyPI and Ref API in firewall
    Add pypi.org, files.pythonhosted.org for uv package management
    Add api.ref.tools for Ref MCP documentation lookup

--- .devcontainer/init-firewall.sh
+++ .devcontainer/init-firewall.sh
@@ -72,7 +72,10 @@ for domain in \
     "statsig.com" \
     "marketplace.visualstudio.com" \
     "vscode.blob.core.windows.net" \
-    "update.code.visualstudio.com"; do
+    "update.code.visualstudio.com" \
+    "pypi.org" \
+    "files.pythonhosted.org" \
+    "api.ref.tools"; do
     echo "Resolving $domain..."
     ips=$(dig +noall +answer A "$domain" | awk '$4 == "A" {print $5}')
     if [ -z "$ips" ]; then
